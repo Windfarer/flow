36d6d01bad5c7d6d1ccde55acf2c61e77b5de206
Eric Yang <windfarer@gmail.com>
2015-05-08 16:15:37 +0800

    remove unused module

--- app/auth.py
+++ app/auth.py
@@ -1,32 +1,10 @@
 from flask import jsonify, g, current_app, request
-from flask.ext.httpauth import HTTPBasicAuth
 
 from .models.user import User
 from .decorators import json
 from jwt_auth import HTTPJWTAuth
 
-
-auth = HTTPBasicAuth()
 auth_token = HTTPJWTAuth()
-
-
-@auth.verify_password
-def verify_password(username, password):
-    g.user = current_app.mongodb_conn.User.find_one({"username": username})
-    if g.user is None:
-        return False
-    return g.user.verify_password(password)
-
-
-@auth.error_handler
-@json
-def unauthorized():
-    response = {
-        "status": 401,
-        "error": "unauthorized",
-        "message": "please authenticate"
-    }
-    return response, 401
 
 
 @auth_token.verify_token
